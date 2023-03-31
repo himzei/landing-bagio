21bae91baefcc3f73c832a9f35d812937df28977
조현일 <himzei@gmail.com>
2023-04-01 08:36:48 +0900

    modal

--- src/components/speciality/Speciality.jsx
+++ src/components/speciality/Speciality.jsx
@@ -4,6 +4,9 @@ import Image1 from "./images/1.jpg";
 import Image2 from "./images/2.jpg";
 import Image3 from "./images/3.jpg";
 import Image4 from "./images/4.jpg";
+import Modal1 from "./images/modal1.jpg";
+import Modal2 from "./images/modal2.jpg";
+import Modal3 from "./images/modal3.jpg";
 
 export default function Speciality() {
   return (
@@ -16,9 +19,21 @@ export default function Speciality() {
         bg="rgba(0, 0, 0, 0.1)"
       />
       <Grid w="5xl" templateColumns={"repeat(3, 1fr)"} zIndex={9}>
-        <SpecialityItem textTitle="수업 구성" svgUrl={Image1} />
-        <SpecialityItem textTitle="일일 일정표" svgUrl={Image2} />
-        <SpecialityItem textTitle="연수 일정" svgUrl={Image3} />
+        <SpecialityItem
+          textTitle="수업 구성"
+          svgUrl={Image1}
+          modalUrl={Modal1}
+        />
+        <SpecialityItem
+          textTitle="일일 일정표"
+          svgUrl={Image2}
+          modalUrl={Modal2}
+        />
+        <SpecialityItem
+          textTitle="연수 일정"
+          svgUrl={Image3}
+          modalUrl={Modal3}
+        />
       </Grid>
     </VStack>
   );
--- src/components/speciality/SpecialityItem.jsx
+++ src/components/speciality/SpecialityItem.jsx
@@ -1,67 +1,113 @@
-import { Box, GridItem, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  GridItem,
+  HStack,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Text,
+  VStack,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
-export default function SpecialityItem({ textTitle, svgUrl }) {
-  return (
-    <GridItem align="center" rounded="full" overflow="hidden">
-      <VStack h="auto" role="group" bg="custom.100" w="320px">
-        <Box w="full" h="56" overflow={"hidden"}>
-          <Image
-            transition="0.4s"
-            _groupHover={{
-              transform: "scale(1.2)",
-            }}
-            w="full"
-            h="56"
-            objectFit={"cover"}
-            objectPosition="center"
-            src={svgUrl}
-          />
-        </Box>
-        <Box
-          w="full"
-          h="28"
-          py={2}
-          px={2}
-          position="relative"
-          overflow={"hidden"}
-        >
-          <Box
-            bg="gray.800"
-            position="absolute"
-            w="full"
-            h="full"
-            top="0"
-            left="0"
-            transition="0.4s"
-            _groupHover={{
-              top: "160px",
-            }}
-          />
-          <Box
-            position="absolute"
-            bottom={-5}
-            right={-5}
-            w="30px"
-            h="30px"
-            bg="transparent"
-            transform={"rotate(45deg) scale(2)"}
-          />
-          <HStack
-            position="absolute"
-            top="4"
-            left="50%"
-            transform={"translateX(-50%)"}
-            cursor={"pointer"}
-          >
-            <Text fontSize={24} fontWeight="600" color="white">
-              {textTitle}
-            </Text>
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-            <HiOutlineArrowNarrowRight color="white" />
-          </HStack>
-        </Box>
-      </VStack>
-    </GridItem>
+export default function SpecialityItem({ textTitle, svgUrl, modalUrl }) {
+  useEffect(() => {
+    AOS.init();
+  });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <GridItem
+        align="center"
+        rounded="full"
+        overflow="hidden"
+        data-aos="fade-in"
+      >
+        <VStack h="auto" role="group" bg="custom.100" w="320px">
+          <Box w="full" h="56" overflow={"hidden"}>
+            <Image
+              transition="0.4s"
+              _groupHover={{
+                transform: "scale(1.2)",
+              }}
+              w="full"
+              h="56"
+              objectFit={"cover"}
+              objectPosition="center"
+              src={svgUrl}
+            />
+          </Box>
+          <Box
+            w="full"
+            h="28"
+            py={2}
+            px={2}
+            position="relative"
+            overflow={"hidden"}
+          >
+            <Box
+              bg="gray.800"
+              position="absolute"
+              w="full"
+              h="full"
+              top="0"
+              left="0"
+              transition="0.4s"
+              _groupHover={{
+                top: "160px",
+              }}
+            />
+            <Box
+              position="absolute"
+              bottom={-5}
+              right={-5}
+              w="30px"
+              h="30px"
+              bg="transparent"
+              transform={"rotate(45deg) scale(2)"}
+            />
+            <HStack
+              position="absolute"
+              top="4"
+              left="50%"
+              transform={"translateX(-50%)"}
+              cursor={"pointer"}
+              onClick={onOpen}
+            >
+              <Text fontSize={24} fontWeight="600" color="white">
+                {textTitle}
+              </Text>
+
+              <HiOutlineArrowNarrowRight color="white" />
+            </HStack>
+          </Box>
+        </VStack>
+        <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+              <Image src={modalUrl} />
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </GridItem>
+    </>
   );
 }
